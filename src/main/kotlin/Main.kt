859fa10eae987839d1efd2742434895d7246ab56
sikora <sikorski.jakubjan@gmail.com>
2025-04-03 15:27:04 +0200

    went back on tab conversion because it was causing text to not be selectable

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,21 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.OffsetMapping
@@ -53,64 +57,84 @@ fun getColoredText(input: String): AnnotatedString {
 }
 
 @Composable
-fun HighlitableTextArea(text:TextFieldValue,changeText:(TextFieldValue)->Unit) {
-    BasicTextField(
+fun HighlitableTextArea(text: String, changeText: (String) -> Unit) {
+    TextField(
         value = text,
-        onValueChange = {
-            val modifiedText = it.text.replace("\t",TAB_SPACE_AMOUNT);
-            var newCursor = it.selection.start;
-            if(modifiedText.length >it.text.length) {
-                newCursor +=3;
-            }
-            changeText(TextFieldValue(text=modifiedText, selection = TextRange(newCursor)))
+        onValueChange = {it->
+            changeText(it)
+//            val modifiedText = it.text.replace("\t", TAB_SPACE_AMOUNT);
+//
+//            if (modifiedText.length > it.text.length) {
+//                var newCursor = it.selection.start;
+//                newCursor += 3;
+//                changeText(TextFieldValue(text = modifiedText, selection = TextRange(newCursor)))
+//            }
+//            else{
+//                changeText(TextFieldValue(text = modifiedText,selection=TextRange(modifiedText.length)))
+//            }
         },
-        textStyle = TextStyle(color = Color.Transparent, fontSize = 12.sp),
+        textStyle = TextStyle(color = Color.Black, fontSize = 12.sp),
         modifier = Modifier
             .fillMaxWidth().height(500.dp)
             .padding(16.dp),
         visualTransformation = {
             TransformedText(
-                getColoredText(text.text).subSequence(0, text.text.length),
+                getColoredText(text).subSequence(0, text.length),
                 OffsetMapping.Identity
             )
-        }
+        },interactionSource = remember { MutableInteractionSource() }, // Ensures interactivity and text selection
     )
 }
 
-fun runScript(script:String){
-    val filename="/home/sikora/IdeaProjects/KotlinEditor/script.kts"
-    File(filename).writeText(script);
-    val process = ProcessBuilder(listOf("kotlinc" ,"-script",filename)  ).redirectErrorStream(true).start();
-    val inputStream = process.inputStream;
-    val reader = BufferedReader(InputStreamReader(inputStream));
-    Thread{
+fun runScript(script: String, modifyOutputText: (String) -> Unit, modifyScriptRunning: (Boolean, Int?) -> Unit) {
+    Thread {
+        val filename = "/home/sikora/IdeaProjects/KotlinEditor/script.kts"
+        File(filename).writeText(script);
+        val process = ProcessBuilder(listOf("kotlinc", "-script", filename)).redirectErrorStream(true).start();
+        modifyScriptRunning(true, null);
+        val inputStream = process.inputStream;
+        val reader = BufferedReader(InputStreamReader(inputStream));
         reader.forEachLine {
-            println(it)
+            modifyOutputText(it);
         }
+        val exitCode = process.waitFor()
+        modifyScriptRunning(false, exitCode);
     }.start()
-    process.waitFor()
+
+
 }
+
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        val scriptText = rememberSaveable { mutableStateOf(TextFieldValue("")) }
+        val scriptText = rememberSaveable { mutableStateOf("") }
+        val outputText = rememberSaveable { mutableStateOf("") }
+        val exitCode: MutableState<Int?> = rememberSaveable { mutableStateOf(null) }
+        val isScriptRunning = rememberSaveable { mutableStateOf(false) }
 
         Column {
             Row {
-                Button(onClick = { runScript(scriptText.value.text) }) {
+                Button(onClick = {
+                    runScript(
+                        scriptText.value,
+                        { output -> outputText.value += output;outputText.value += "\n" },
+                        { running, code -> isScriptRunning.value = running;exitCode.value = code })
+                }) {
                     Text("Run Script")
                 }
+                Text("Is script running: ${isScriptRunning.value}")
+                Text("Return Code: ${exitCode.value}")
             }
             Row(modifier = Modifier.fillMaxSize()) {
                 Column(modifier = Modifier.fillMaxWidth(0.5f)) {
                     Row(modifier = Modifier.fillMaxWidth()) {
-                        HighlitableTextArea(scriptText.value,changeText = {it->scriptText.value =it });
+                            HighlitableTextArea(scriptText.value, changeText = { it -> scriptText.value = it });
                     }
                 }
                 Column(modifier = Modifier.fillMaxWidth().background(Color.Black).fillMaxHeight()) {
                     Text(
-                        "this is a veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery long text",
+                        outputText.value,
                         color = Color.White,
                         fontSize = 12.sp
                     )

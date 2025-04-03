0ed3f534b6a6e9c086f6f45a073b070fa403f73b
sikora <sikorski.jakubjan@gmail.com>
2025-04-03 13:29:43 +0200

    automatically change tabs to spaces as tabs don't display correctly

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,16 +2,20 @@ import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.*
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -19,12 +23,14 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
-val keywords: Map<String, Color> = mapOf("val" to Color.Blue);
+const val TAB_SPACE_AMOUNT = "    "; // 1 tab is 4 spaces
+val keywords: Map<String, Color> =
+    mapOf("val" to Color.Blue, "var" to Color.Blue, "if" to Color.Blue, "for" to Color.Blue, "fun" to Color.Blue);
 
 fun getColoredText(input: String): AnnotatedString {
     return buildAnnotatedString {
         val regex = Regex("([^\\s;]+)|([\\s;]+)") // Captures words and all whitespaces including ;
-        val matches = regex.findAll(input)
+        val matches = regex.findAll(input);
 
         for (match in matches) {
             val part = match.value
@@ -43,44 +49,26 @@ fun getColoredText(input: String): AnnotatedString {
     }
 }
 
-fun BuildColoredWords(text: String): AnnotatedString {
-    var startIndex = 0;
-    val annotatedString = buildAnnotatedString {
-        if (text == "") {
-            return@buildAnnotatedString;
-        }
-        val split = text.split(" ");
-        split.forEachIndexed { index, s ->
-            if (keywords.containsKey(s)) {
-                withStyle(style = SpanStyle(color = keywords[s]!!, fontSize = 12.sp)) {
-                    append(s);
-                    append(" ");
-                }
-            } else {
-                withStyle(style = SpanStyle(color = Color.Black, fontSize = 12.sp)) {
-                    append(s);
-                    append(" ");
-                }
-            }
-
-        }
-    }
-    return annotatedString;
-}
-
 @Composable
 fun HighlitableTextArea() {
-    val text = rememberSaveable { mutableStateOf("") }
+    val text = rememberSaveable { mutableStateOf(TextFieldValue("")) }
     BasicTextField(
         value = text.value,
-        onValueChange = { text.value = it },
+        onValueChange = {
+            val modifiedText = it.text.replace("\t",TAB_SPACE_AMOUNT);
+            var newCursor = it.selection.start;
+            if(modifiedText.length >it.text.length) {
+                newCursor +=3;
+            }
+            text.value = TextFieldValue(text=modifiedText, selection = TextRange(newCursor))
+        },
         textStyle = TextStyle(color = Color.Transparent, fontSize = 12.sp),
         modifier = Modifier
             .fillMaxWidth().height(500.dp)
             .padding(16.dp),
         visualTransformation = {
             TransformedText(
-                getColoredText(text.value).subSequence(0, text.value.length),
+                getColoredText(text.value.text).subSequence(0, text.value.text.length),
                 OffsetMapping.Identity
             )
         }
@@ -91,22 +79,27 @@ fun HighlitableTextArea() {
 @Composable
 @Preview
 fun App() {
-
     MaterialTheme {
-        Row(modifier = Modifier.fillMaxSize()) {
-            Column(modifier = Modifier.fillMaxWidth(0.5f)) {
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    HighlitableTextArea();
+        Column {
+            Row {
+
+            }
+            Row(modifier = Modifier.fillMaxSize()) {
+                Column(modifier = Modifier.fillMaxWidth(0.5f)) {
+                    Row(modifier = Modifier.fillMaxWidth()) {
+                        HighlitableTextArea();
+                    }
+                }
+                Column(modifier = Modifier.fillMaxWidth().background(Color.Black).fillMaxHeight()) {
+                    Text(
+                        "this is a veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery long text",
+                        color = Color.White,
+                        fontSize = 12.sp
+                    )
+
                 }
             }
-            Column(modifier = Modifier.fillMaxWidth().background(Color.Black).fillMaxHeight(),) {
-                Text("this is a veeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeery long text",color = Color.White,fontSize = 12.sp)
-
-            }
         }
-
-
-
     }
 }
 
